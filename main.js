2697222c8e70c48701dcdb9e359f7b8d799fba76
Niel-nts <110512392+Niel-nts@users.noreply.github.com>
2022-10-18 22:51:21 -0300

    Atualizações

--- main.js
+++ main.js
@@ -4,36 +4,42 @@ let randomNumber= Math.round(Math.random()*10)
 let xAttemps = 1
 const btnTry = document.querySelector("#btnTry")
 const btnReset = document.querySelector("#btnReset")
+const inputNumber = document.querySelector("#inputNumber")
 
 //Eventos 
-btnTry.addEventListener('click', handleTryClick)
+btnTry.addEventListener('click', checkInput)
 btnReset.addEventListener('click', handleResetClick)
 document.addEventListener('keydown', keyDownEvent)
 
 //Funções callback
-function handleTryClick(event){
+function checkInput(event){
     event.preventDefault() //necessário para que a página não seja recarregada! Isso ocorre por que o padrão do "form" no html é enviar o formulário!
-    const inputNumber = document.querySelector("#inputNumber")
+    if ((inputNumber.value=="" && not(screen1.classList.contains('hide'))) || Number(inputNumber.value) < 0 || Number(inputNumber.value) > 10 || (!Number(inputNumber.value) && Number(inputNumber.value)!=0)){
+        alert("Por favor, insira um número válido de 0 a 10")
+        inputNumber.focus()
+        inputNumber.value = ""
 
-    if (Number(inputNumber.value) < 0 || Number(inputNumber.value) > 10 || (!Number(inputNumber.value) && Number(inputNumber.value)!=0)) {
-        alert("Por favor insira um número de 0 a 10!")        
-    } 
+    } else if (screen2.classList.contains('hide')){
+        handleTryClick() 
+    }  
+}
 
+function handleTryClick(){
     if (Number(inputNumber.value) == randomNumber) {
         toggleScreen()
         screen2.querySelector("h2").innerText = `Acertou em ${xAttemps} tentativas`
+    } else {
+        xAttemps++    
     }
-
-    
-
     inputNumber.value = "" //torna a caixa de preenchimento vazia para a próxima tentativa
-    xAttemps++    
+    inputNumber.focus()
 }
 
 function handleResetClick(){
     toggleScreen()
     xAttemps = 1
-    let randomNumber= Math.round(Math.random()*10)
+    randomNumber= Math.round(Math.random()*10)
+    inputNumber.focus()
 }
 
 function toggleScreen(){
@@ -44,6 +50,6 @@ function toggleScreen(){
 function keyDownEvent(e){
     if (e.key == 'Enter' && screen1.classList.contains('hide')) {
         handleResetClick()
-    }
+    }     
 }
 
